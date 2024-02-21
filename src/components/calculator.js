234636bae72d88301158a345080b390d8363e972
Antonykibet <antonykibet059@gmail.com>
2024-02-21 10:55:26 +0300

    Completed calculator func

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,24 +1,43 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+
 function Calculator(){
-    const [display, setDisplay] = useState(0)
+    const result = useRef(null)
     const [input,setInput] = useState(0)
-    const [result,setResult] = useState(0)
-    function updateDisplays(e){
-        setDisplay(e.target.value)
+    function sum(e){
+        e.preventDefault()
+        setInput((prev)=>{
+            return prev + Number(result.current.value)
+        })
     }
-    function sum(){
-        
-        setResult(display+input)
+    function substract(e){
+        e.preventDefault()
+        setInput((prev)=>{
+            return prev - Number(result.current.value)
+        })
+    }
+    function multiplication(e){
+        e.preventDefault()
+        setInput((prev)=>{
+            return prev ** Number(result.current.value)
+        })
+    }
+    function division(e){
+        e.preventDefault()
+        setInput((prev)=>{
+            return prev / Number(result.current.value)
+        })
     }
     return(
         <div>
-            <input type="text" value={display}/>
-            <input type="text" value={input} onChange={updateDisplays} />
-            <input type="text" value={result} />
-            <button onClick={sum}>Sum</button>
-            <button>Substract</button>
-            <button>Multiplication</button>
-            <button>Division</button>
+            <form>
+                <h3>Calculator</h3>
+                <p >{Number(input)}</p>
+                <input ref={result} type='number' />
+                <button onClick={sum}>Sum</button>
+                <button onClick={substract}>Substract</button>
+                <button onClick={multiplication}>Multiplication</button>
+                <button onClick={division}>Division</button>
+            </form>
         </div>
     )
 }
