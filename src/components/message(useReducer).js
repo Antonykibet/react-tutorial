1bff1fe7d5301832b1ea1bfd4b4deeb93cd29a2a
Antonykibet <antonykibet059@gmail.com>
2024-02-26 23:53:48 +0300

    added message property in action object

--- src/components/message(useReducer).js
+++ src/components/message(useReducer).js
@@ -1,33 +1,60 @@
 import { useReducer } from "react"
+
 function reducerFunc(state,action){
     switch(action.name){
         case 'Antony':{
-            return [...state,{name:'Antony'}]
+            return state.map((item)=>{
+                if(item.name === action.name){
+                    item.message = action.message
+                }
+                return item
+            })
         }
         case 'Angie':{
-            return [...state,{name:'Angie'}]
+            return state.map((item)=>{
+                if(item.name === action.name){
+                    item.message = action.message
+                }
+                return item
+            })
         }
         case 'Lynn':{
-            return [...state,{name:'Lynn'}]
+            return state.map((item)=>{
+                if(item.name === action.name){
+                    item.message = action.message
+                }
+                return item
+            })
         }
     }
 }
 function Chatbox(){
-    const [state,dispatcher] = useReducer(reducerFunc,{id:0, name:'Antony'})
+    const [state,dispatcher] = useReducer(reducerFunc,[{id:0, name:'Antony',message:''},{id:0, name:'Angie',message:''},{id:0, name:'Lynn',message:''}])
     return(
         <>
-            <button onClick={()=>{
-                dispatcher({name:'Antony'})
-            }}>Antony</button>
-            <button onClick={()=>{
-                dispatcher({name:'Angie'})
-            }}>Angie</button>
             <button 
-                onClick={()=>{
-                dispatcher({name:'Lynn'})
-            }}>Lynn</button>
-            <textarea >Hello</textarea>
-            <button>send to: {state.name}</button>
+                onClick={(e)=>{
+                dispatcher(
+                    {name:'Antony',
+                    message:e.target.value
+            })
+            }}>Antony</button><br/>
+            <button onClick={(e)=>{
+                dispatcher({name:'Angie',message:e.target.value})
+            }}>Angie</button><br/>
+            <button 
+                onClick={(e)=>{
+                dispatcher({name:'Lynn',message:e.target.value})
+            }}>Lynn</button><br/>
+            <textarea 
+                value={state[0].message} 
+                onChange={
+                    (e)=>{
+                        dispatcher({name:state[0].name,message:e.target.value})
+                    }
+                }>
+                </textarea><br/>
+            <button>send to: {state[0].name}</button>
         </>
     )
 }
