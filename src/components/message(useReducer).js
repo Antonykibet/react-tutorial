dc69c2a586fb6369834928ccb1543d5c160aa79e
Antonykibet <antonykibet059@gmail.com>
2024-02-28 08:06:38 +0300

    added chat history in chat box(useReducer example)

--- src/components/message(useReducer).js
+++ src/components/message(useReducer).js
@@ -1,27 +1,33 @@
-import { useReducer } from "react"
+import { useReducer, useRef } from "react"
 
 function reducerFunc(state,action){
     switch(action.name){
         case 'Antony':{
             return state.map((item)=>{
+                item.selected=false
                 if(item.name === action.name){
                     item.message = action.message
+                    item.selected=true
                 }
                 return item
             })
         }
-        case 'Angie':{
+        case 'Kibet':{
             return state.map((item)=>{
+                item.selected=false
                 if(item.name === action.name){
                     item.message = action.message
+                    item.selected=true
                 }
                 return item
             })
         }
-        case 'Lynn':{
+        case 'Ruto':{
             return state.map((item)=>{
+                item.selected=false
                 if(item.name === action.name){
                     item.message = action.message
+                    item.selected=true
                 }
                 return item
             })
@@ -29,32 +35,64 @@ function reducerFunc(state,action){
     }
 }
 function Chatbox(){
-    const [state,dispatcher] = useReducer(reducerFunc,[{id:0, name:'Antony',message:''},{id:0, name:'Angie',message:''},{id:0, name:'Lynn',message:''}])
+    const [state,dispatcher] = useReducer(reducerFunc,[{id:0, name:'Antony',message:'fdfdf',selected:false},{id:0, name:'Kibet',message:'',selected:false},{id:0, name:'Ruto',message:'',selected:false}])
+    const textInput = useRef(null)
     return(
         <>
             <button 
                 onClick={(e)=>{
-                dispatcher(
-                    {name:'Antony',
-                    message:e.target.value
-            })
+                state.forEach((item)=>{
+                    if(item.name===e.target.innerText){ 
+                        textInput.current.value = item.message
+                    }
+                })
+                dispatcher({
+                        name:'Antony',
+                        message:textInput.current.value
+                    }
+                )
             }}>Antony</button><br/>
             <button onClick={(e)=>{
-                dispatcher({name:'Angie',message:e.target.value})
-            }}>Angie</button><br/>
+                state.forEach((item)=>{
+                    if(item.name===e.target.innerText){
+                        textInput.current.value = item.message
+                    }
+                })
+                dispatcher({name:'Kibet',message:textInput.current.value})
+            }}>Kibet</button><br/>
             <button 
                 onClick={(e)=>{
-                dispatcher({name:'Lynn',message:e.target.value})
-            }}>Lynn</button><br/>
+                    state.forEach((item)=>{
+                        if(item.name===e.target.innerText){
+                            textInput.current.value = item.message
+                        }
+                    }) 
+                dispatcher({name:'Ruto',message:textInput.current.value})
+            }}>Ruto</button><br/>
             <textarea 
-                value={state[0].message} 
+                ref={textInput}
+                value={state.forEach(item=>{
+                    if(item.selected === true){
+                        return item.message
+                    }
+                })} 
                 onChange={
                     (e)=>{
-                        dispatcher({name:state[0].name,message:e.target.value})
+                        state.forEach((item)=>{
+                            if(item.selected === true){
+                                dispatcher({name:item.name,message:e.target.value})
+                            }
+                        })
                     }
                 }>
                 </textarea><br/>
-            <button>send to: {state[0].name}</button>
+            <button>send to: {state.forEach(item=>{
+                if(item.selected === true){
+                    console.log(state)
+                    return item.name
+                }
+            })
+            }</button>
         </>
     )
 }
